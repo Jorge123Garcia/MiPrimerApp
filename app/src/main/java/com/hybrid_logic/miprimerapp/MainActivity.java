e0146478f03ca233c8f8e13c2f0c0324c3e9f5a0
Jorge123Garcia <jorgeagcl217@gmail.com>
2024-05-29 18:17:31 -0600

    Commit Message with no value2

--- app/src/main/java/com/hybrid_logic/miprimerapp/MainActivity.java
+++ app/src/main/java/com/hybrid_logic/miprimerapp/MainActivity.java
@@ -12,31 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int matematicas =5;
-        int quimica = 5;
-        int fisica =5;
-        int promedio =0;
 
-        promedio =(matematicas+quimica+fisica)/3;
-
-
-        if(promedio >=6){
-
-
-            Toast.makeText(this, "Aprobado", Toast.LENGTH_SHORT).show();
-            
-        }else if(promedio<=5){
-           Toast.makeText(this, "Reprobado", Toast.LENGTH_SHORT).show();
-
-           
-        }
 
 
     }
-
-
-
-
 
 
 }
